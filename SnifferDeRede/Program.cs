318a4de41569cb5cf2b2ac72e8e11c4d3878516c
Matheus Ferreira <matheus.44300@gmail.com>
2022-11-22 21:15:33 -0300

    Added progress bar

--- SnifferDeRede/Program.cs
+++ SnifferDeRede/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace SnifferDeRede
 {
@@ -15,9 +16,21 @@ namespace SnifferDeRede
 
             try
             {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Carregando o trojan");
+                for (int i = 0; i < 100; i++)
+                {
+                    Thread.Sleep(100);
+                    DrawTextProgressBar(i, 100);
+                }
+
                 app.Start();
                 while (true)
                 {
+                    Console.Write("Seus dados são nossos agora ");
+                    Console.Write("Obrigado pelas senhas ");
+                    Console.Write("Sandrinha e Paulo juntos ");
+                    Console.Write("Spy Spy Spy ");
                 }
             }
             catch (Exception ex)
@@ -30,6 +43,39 @@ namespace SnifferDeRede
             //Application.EnableVisualStyles();
             //Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new MJsnifferForm());
+        }
+
+        private static void DrawTextProgressBar(int progress, int total)
+        {
+            //draw empty progress bar
+            Console.CursorLeft = 0;
+            Console.Write("["); //start
+            Console.CursorLeft = 32;
+            Console.Write("]"); //end
+            Console.CursorLeft = 1;
+            float onechunk = 30.0f / total;
+
+            //draw filled part
+            int position = 1;
+            for (int i = 0; i < onechunk * progress; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.CursorLeft = position++;
+                Console.Write(" ");
+            }
+
+            //draw unfilled part
+            for (int i = position; i <= 31; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.Gray;
+                Console.CursorLeft = position++;
+                Console.Write(" ");
+            }
+
+            //draw totals
+            Console.CursorLeft = 35;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Write(progress.ToString() + " of " + total.ToString() + "    "); //blanks at the end remove any excess
         }
     }
 }
